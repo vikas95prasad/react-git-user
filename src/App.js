512de1fddc9b39cffac02d8a282118145be163c9
vikas95prasad <vikas95prasad@gmail.com>
2019-12-01 01:02:57 +0530

    Add particle js backgroud

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particleParams = {
+                        "particles": {
+                            "number": {
+                                "value": 1000,
+                                "density": {
+                                    "enable": true,
+                                    "value_area": 15000
+                                }
+                            }
+                          }
+                        }
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Particles params={particleParams} />
+      </div>
+    );
+  }
 }
 
 export default App;
